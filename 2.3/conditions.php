cb396e239e5e0c1d97feaccaf8d32045ce529999
Karcoolka <simkova54321@gamil.com>
2023-05-15 10:23:41 +0100

    php web server config + basic syntax pick uú

---
+++ 2.3/conditions.php
@@ -0,0 +1,16 @@
+<?php  //can stay open if file is only for php (not for html etd.)
+
+$result = 1 < 3;//true, if 1 is less then 3
+
+echo $result == true; // if false, not displaied¨
+
+$first_name = 'Jeremy';
+$second_name = 'McPeak';
+
+if($first_name == 'jeremy' && $second_name == 'MyPeak' ){
+    echo 'Condition is true';
+} else {
+    echo 'Condition is false';
+}
+
+
---
+++ 2.3/error.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+<h1>Title</h1>
+    <?php 
+    $name = 'Jeremy'; //variable with $
+    echo 'Hello, ' . $name; // concatinating instead of + we use: .
+    echo "Hello, $name ; //or with double quotes
+    ;?>    
+</body>
+</html>
